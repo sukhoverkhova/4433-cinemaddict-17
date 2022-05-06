00d11ca6554f65d3ba53aa5baf46bc93081d9a4c
sukhoverkhova <sukhoverkhova.an@gmail.com>
2022-05-06 20:49:45 +0300

    Открывает деталии фильма по клику на карточку

--- src/presenter/films-list-presenter.js
+++ src/presenter/films-list-presenter.js
@@ -3,6 +3,7 @@ import FilmsListSectionView from '../view/films-list-section-view.js';
 import FilmsListHeaderView from '../view/films-list-header-view.js';
 import FilmsListView from '../view/films-list-view.js';
 import FilmView from '../view/film-view.js';
+import FilmDetailsView from '../view/film-details-view.js';
 import SortView from '../view/sort-view.js';
 import ShowMoreButtonView from '../view/show-more-button-view.js';
 import {render} from '../render.js';
@@ -30,10 +31,36 @@ export default class FilmListPresenter {
     render(this.#filmsListComponent, this.#filmsListSectionComponent.element);
 
     for (let i = 0; i < this.#films.length; i++) {
-      const filmView = new FilmView(this.#films[i]);
-      render(filmView, this.#filmsListComponent.element);
+      this.#renderFilm(this.#films[i]);
     }
 
     render(new ShowMoreButtonView(), this.#mainContainer);
+    // render(new FilmDetailsView(this.#films[0]), this.#mainContainer);
+  };
+
+  #renderFilm = (film) => {
+    const filmComponent = new FilmView(film);
+    let filmDetailsComponent;
+
+    const hideFilmDetails = () => {
+      filmDetailsComponent.element.remove();
+    };
+
+    const showFilmDetails = () => {
+      filmDetailsComponent = new FilmDetailsView(film);
+
+      const oldFilmDetailsElement = document.querySelector('.film-details');
+      if (oldFilmDetailsElement) {
+        oldFilmDetailsElement.remove();
+      }
+
+      render(filmDetailsComponent, this.#mainContainer);
+
+      filmDetailsComponent.element.querySelector('.film-details__close-btn').addEventListener('click', hideFilmDetails);
+    };
+
+    filmComponent.element.addEventListener('click', showFilmDetails);
+
+    render(filmComponent, this.#filmsListComponent.element);
   };
 }
